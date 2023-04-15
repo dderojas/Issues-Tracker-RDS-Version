f368ca75901bcb78bd60145f59e5e1e2c107541d
Don de Rojas <derojasdonny@gmail.com>
2023-04-14 17:08:07 -0700

    figuring out rds connection from lambda

--- server/index.js
+++ server/index.js
@@ -1,7 +1,33 @@
+const { Client } = require('pg')
+
+// const client = new Client({
+//   user: '<your db username>',
+//   host: '<your endpoint>',
+//   database: '<your database name>',
+//   password: '<your database password>',
+//   port: 5432
+// });
+
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
   let responseMessage = 'Hello, World!';
+  console.log(process.env, 'heeellllpppp')
 
+  // try {
+
+  //   await client.connect();
+  //   console.log("Connected Successfully");
+  //   //your code here
+
+  // } catch (err) {
+
+  //     console.log("Failed to Connect Successfully");
+  //     throw err;
+  //     //error message
+  // }
+
+  // client.end();
+  
   return {
     statusCode: 200,
     headers: {
