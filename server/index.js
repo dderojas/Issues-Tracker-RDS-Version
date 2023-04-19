db7eef67df4945b8b495cf68c95c28b6d563aa3b
Don de Rojas <derojasdonny@gmail.com>
2023-04-19 12:58:00 -0700

    working on getting basic GET and POST func

--- server/index.js
+++ server/index.js
@@ -11,17 +11,62 @@ const client = new Client({
 module.exports.handler = async (event, callback) => {
   console.log('Event: ', event);
   let responseMessage = 'Hello, World!';
-  console.log(process.env.DB_INSTANCE_ADDRESS, 'db instance address!')
+  console.log(process.env, 'db instance address!')
 
+  let createTable = `CREATE TABLE IF NOT EXISTS accounts (
+    user_id serial PRIMARY KEY,
+    name VARCHAR ( 50 ) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+          last_login TIMESTAMP 
+  )`
+
+  let getRecord = `SELECT * FROM "accounts" LIMIT 1;`
+
+  // let values = event.queryStringParameters['Name'] || 'nothing'
 
   console.log(client, 'client info!')
+
   try {
     client.connect((err) => {
       if (err) callback(err);
       else callback(null, 'Success');
     });
 
+    client.query(createTable, (err) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log('table created')
+      }
+    })
+
     console.log("Connected Successfully");
+
+    if (event.queryStringParameters && event.queryStringParameters['Name']) {
+      responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+
+      let postRecord = `INSERT INTO accounts(name) VALUES (${event.queryStringParameters['Name']}) RETURNING *;`
+
+      client.query(postRecord, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log(res.rows[0])
+        }
+      })
+      
+    }
+
+    if (event.httpMethod['GET']) {
+      client.query(getRecord, (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          console.log(res.rows[0])
+        }
+      })
+    }
+
   } catch (err) {
 
       console.log("Failed to Connect Successfully");
